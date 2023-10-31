7327c63212665782c2ddb53309987ffd01106dd1
Ariela Israel <arielaisrael12@gmail.com>
2023-10-31 14:04:02 -0400

    complete

--- src/suppliers/suppliers.service.js
+++ src/suppliers/suppliers.service.js
@@ -15,7 +15,8 @@ function update(updatedSupplier) {
 	return knex('suppliers')
 		.select('*')
 		.where({ supplier_id: updatedSupplier.supplier_id })
-		.update(updatedSupplier, '*');
+		.update(updatedSupplier, '*')
+		.then(createdRecords => createdRecords[0]);
 }
 
 function destroy(supplier_id) {
